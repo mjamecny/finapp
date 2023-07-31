3a1bb7d24cfd3a7f0848b4fbdd4c527b7250b78f
mjamecny <jamecny.m@tutanota.com>
2023-07-31 21:03:02 +0200

    add filtering transactions by description

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,17 +1,39 @@
 import { useUser } from "../features/authentication/useUser"
-import TransactionList from "../features/transactions/TransactionList"
 import { useTransactions } from "../features/transactions/useTransactions"
+
+import styled from "styled-components"
+
+import TransactionList from "../features/transactions/TransactionList"
 import ButtonBack from "../ui/ButtonBack"
+import Input from "../ui/Input"
+import { useState } from "react"
+
+const StyledTransactions = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export default function Transactions() {
+  const [query, setQuery] = useState("")
   const { user } = useUser()
   const userId = user?.id
   const { isLoading, transactions, error } = useTransactions(userId)
 
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(query.toLowerCase())
+  )
+
   return (
-    <div>
-      <TransactionList userId={userId} transactions={transactions} />
+    <StyledTransactions>
+      <Input
+        type="text"
+        value={query}
+        placeholder="Search by description"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <TransactionList userId={userId} transactions={filteredTransactions} />
       <ButtonBack />
-    </div>
+    </StyledTransactions>
   )
 }
